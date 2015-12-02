2b538108cc9f4de79453aa3ae283f7a6c231f073
Mathasyu <test@tmc2000.de>
2015-12-02 15:07:48 +0100

    update main.c
    Verbesserung: checkZahl() prüft, ob die Zahl zwischen 0 und 1 liegt

--- BlitzPrakt02-3/main.c
+++ BlitzPrakt02-3/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+int checkZahl(float n) {
+    int rueckgabewert = 2;
+    if ((n < 1) && (n > 0)) {
+        rueckgabewert = 1;
+    }
+    return rueckgabewert;
+}
 int forSchleife(float n, int m){
-        /*float n;
-        int m;
-        printf("Kommazahl zwischen 0 und 1 eingeben \n");
-        scanf("%fl", &n);
- 
-        printf("Nachkommastellen eingeben \n");
-        scanf("%d", &m);*/
         printf("For Schleife: \n");
+        printf("0.");
         for (m=m;m>0; m--) {
  
                 n= n * 2;
@@ -17,21 +18,14 @@ int forSchleife(float n, int m){
                 else {
                         printf("0");}
         }
-        getchar();
         printf(" \n\n");
     return 0;
 }
  
 
 int whileSchleife(float n, int m){
-        /*float n;
-        int m;
-        printf("Kommazahl zwischen 0 und 1 eingeben \n");
-        scanf("%fl", &n);
- 
-        printf("Nachkommastellen eingeben \n");
-        scanf("%d", &m);*/
         printf("While Schleife: \n");
+        printf("0.");
         while(m > 0) {
                 m--;
                 n= n * 2;
@@ -41,20 +35,13 @@ int whileSchleife(float n, int m){
                 else {
                         printf("0");}
         }
-        getchar();
         printf("\n\n");
 }
  
 
 int doWhileSchleife(float n, int m){
-        /*float n;
-        int m;
-        printf("Kommazahl zwischen 0 und 1 eingeben \n");
-        scanf("%fl", &n);
- 
-        printf("Nachkommastellen eingeben \n");
-        scanf("%d", &m); */
         printf("DO WHILE Schleife: \n");
+        printf("0.");
         do{
                 m--;
                 n= n * 2;
@@ -64,10 +51,10 @@ int doWhileSchleife(float n, int m){
                 else {
                         printf("0");}
         } while(m > 0);
-        getchar();  
         printf("\n\n");
 }
 int main(void) {
+    int pruefen;
     float n1;
     int m1;
      printf("Kommazahl zwischen 0 und 1 eingeben \n");
@@ -75,8 +62,16 @@ int main(void) {
  
         printf("Nachkommastellen eingeben \n");
         scanf("%d", &m1);
-    forSchleife(n1,m1);
-    whileSchleife(n1,m1);
-    doWhileSchleife(n1,m1);
+     pruefen = checkZahl(n1);
+    if ( pruefen == 1) {
+        printf("\nErgebnis: \n");
+        forSchleife(n1,m1);
+        whileSchleife(n1,m1);
+        doWhileSchleife(n1,m1);
+    } else {
+        printf("Sie haben eine ungültige Zahl angegeben\n");
+    }
+    getchar();
+    getchar();
     return 0;
 }
